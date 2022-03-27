3041757e8610b9ba945524fd45082ab022c8c073
Michael Suzzi <msuz.code@gmail.com>
2022-03-27 21:58:57 +1100

    Comment out perlbrew.fish, was causing errors

--- 1_common/.config/fish/conf.d/msuz.fish
+++ 1_common/.config/fish/conf.d/msuz.fish
@@ -2,10 +2,10 @@ set -xU EDITOR vi
 set -xU SUDO_EDITOR vi
 set -xU BROWSER firefox
 
-if test -f $HOME/perl5/perlbrew/etc/perlbrew.fish
-    source $HOME/perl5/perlbrew/etc/perlbrew.fish
-    clear
-end
+#if test -f $HOME/perl5/perlbrew/etc/perlbrew.fish
+#    source $HOME/perl5/perlbrew/etc/perlbrew.fish
+#    clear
+#end
 
 if test -f $HOME/.config/fish/conf.d/msuz_cubepoot.fish
     source $HOME/.config/fish/conf.d/msuz_cubepoot.fish
