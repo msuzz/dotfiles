b138ee3716a6153416118d69320352e41c3df6c8
Michael Suzzi <msuz.code@gmail.com>
2022-06-22 17:39:49 +1000

    Doom Emacs env vars for bash and fish

--- 1_common/.config/fish/conf.d/msuz.fish
+++ 1_common/.config/fish/conf.d/msuz.fish
@@ -1,11 +1,16 @@
-set -xU EDITOR vi
-set -xU SUDO_EDITOR vi
-set -xU BROWSER firefox
+set -gx EDITOR vi
+set -gx SUDO_EDITOR vi
+set -gx BROWSER firefox
 
 #if test -f $HOME/perl5/perlbrew/etc/perlbrew.fish
 #    source $HOME/perl5/perlbrew/etc/perlbrew.fish
 #    clear
 #end
+
+# Doom Emacs data dirs
+set -gx EMACSDIR ~/.emacs.d
+set -gx DOOMDIR ~/.config/doom
+set -gx DOOMLOCALDIR ~/.emacs.d/.local
 
 if test -f $HOME/.config/fish/conf.d/msuz_wsl.fish
     source $HOME/.config/fish/conf.d/msuz_wsl.fish
