dd1f3a4fd7082ff16e81e11f3efad24baddd625b
Michael Suzzi <msuz.code@gmail.com>
2023-04-12 08:33:01 +1000

    Check for and load perlbrew.fish; Change /home/michael to ~

--- 2_wsl/.config/fish/conf.d/msuz_wsl.fish
+++ 2_wsl/.config/fish/conf.d/msuz_wsl.fish
@@ -6,9 +6,13 @@ if test -d /usr/local/lib/libpsn00b
     fish_add_path /usr/local/mipsel-none-elf/bin
 end
 
-if test -e /home/michael/.config/guix/current
-    set -gx GUIX_PROFILE /home/michael/.config/guix/current
+if test -e ~/.config/guix/current
+    set -gx GUIX_PROFILE ~/.config/guix/current
     bass source $GUIX_PROFILE/etc/profile
+end
+
+if test -e ~/perl5/perlbrew/etc/perlbrew.fish
+    . ~/perl5/perlbrew/etc/perlbrew.fish
 end
 
 # Ensure always at end of PATH
