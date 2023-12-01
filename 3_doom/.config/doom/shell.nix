e9eae164f421e946dc80ec6b3441d488e43f955c
Michael Suzzi <msuz.code@gmail.com>
2023-12-01 21:12:23 +1100

    Update Doomemacs shell.nix with more langauge servers

--- 3_doom/.config/doom/shell.nix
+++ 3_doom/.config/doom/shell.nix
@@ -1,4 +1,26 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    buildInputs = [ pkgs.emacs28NativeComp pkgs.ripgrep pkgs.fd pkgs.fantasque-sans-mono pkgs.crimson pkgs.stdenv pkgs.gcc pkgs.ccls pkgs.cmake pkgs.unzip pkgs.shellcheck pkgs.multimarkdown pkgs.html-tidy];
+    buildInputs = with pkgs; [
+      emacs28NativeComp
+      ripgrep
+      fd
+      fantasque-sans-mono
+      crimson
+      stdenv
+      gcc
+      ccls
+      cmake
+      clang
+      nodejs
+      jdt-language-server
+      java-language-server
+      python311
+      python311Packages.pip
+      python311Packages.virtualenv
+      python311Packages.python-lsp-server
+      unzip
+      shellcheck
+      multimarkdown
+      html-tidy
+    ];
 }
