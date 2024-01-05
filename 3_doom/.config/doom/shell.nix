1871d134b23eef6c9fa7df6e3c2c5bcaebbe1594
Michael Suzzi <msuz.code@gmail.com>
2024-01-05 18:20:19 +1100

    Doomemacs shell.nix: Add required dependencies

--- 3_doom/.config/doom/shell.nix
+++ 3_doom/.config/doom/shell.nix
@@ -12,15 +12,27 @@
       cmake
       clang
       nodejs
+      nodePackages.js-beautify
       jdt-language-server
       java-language-server
       python311
       python311Packages.pip
       python311Packages.virtualenv
       python311Packages.python-lsp-server
+      python311Packages.nose
+      python311Packages.pytest
       unzip
       shellcheck
       multimarkdown
       html-tidy
+      sbcl
+      fpm
+      gfortran
+      php83
+      php83Packages.composer
+      isort
+      pipenv
+      stylelint
+      glslang
     ];
 }
